50d5c8f915b14e24a39e72d5c607bab465f02d71
haojun-mah <mahhaojun03@gmail.com>
2025-06-05 17:49:47 +0800

    Completed linking frontend to backend. Yet to test it

--- andriodApplication/frontend/GroceryPicker/app/(tabs)/groceryHistory.tsx
+++ andriodApplication/frontend/GroceryPicker/app/(tabs)/groceryHistory.tsx
@@ -1,14 +1,27 @@
 import DropdownCard from '@/components/DropdownCard';
+import { useGroceryContext } from '@/context/groceryContext';
 import { Text, View } from 'react-native';
+import { GroceryItem } from '@/context/groceryContext';
 
 const groceryHistory = () => {
+  const { grocery , isLoading, error } = useGroceryContext();
+
+  if (error) {
+    console.log("Front end handling error from grocerycontext. Receiving at groceryHistory");
+  }
+
+  if (!grocery || grocery.length === 0) {
+    throw Error("Invalid grocery list at groceryHistory");
+  }
+
+  const formatGroceryList: string[] = grocery?.map((item: GroceryItem) => { // currently mapping item into array of strings. to map array of items into array of array of strings in the future
+    return `${item.name} - ${item.quantity}${item.unit}`
+  });
+
   return (
     <View className="flex items-center mt-20 p-4 gap-2">
       <Text className="font-bold font-roboto text-2xl">Grocery History</Text>
-      <DropdownCard
-        outsideText="test"
-        insideText={['Egg', 'Chicken']}
-      ></DropdownCard>
+      <DropdownCard outsideText="Metadata in here" insideText={formatGroceryList}/> 
     </View>
   );
 };
---
+++ andriodApplication/frontend/GroceryPicker/context/groceryContext.tsx
@@ -0,0 +1,58 @@
+import React, { useContext, useState } from "react";
+
+interface ErrorResponse {
+  statusCode: number;
+  message: string;
+  details?: string; 
+}
+
+interface GroceryContextType {
+  grocery: GroceryItem[] | null;
+  setGrocery: (grocery: GroceryItem[] | null) => void; // defining types for parameter and return
+  isLoading: boolean;
+  error: ErrorResponse | null;
+  setIsLoading: (loading: boolean) => void;
+  setError: (error: ErrorResponse | null) => void;
+}
+
+export interface GroceryItem {
+    "name" : string;
+    "quantity" : number;
+    "unit" : string;
+}
+
+const GroceryContext = React.createContext<GroceryContextType | undefined>(undefined); // necessary to pass types to createcontext so that the handler of context can know what to expect
+
+export const GroceryContextProvider: any = ({children}: any) => {
+    const [grocery, setGrocery] = useState<GroceryItem[] | null>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<ErrorResponse | null>(null);
+
+
+    const value: GroceryContextType = {
+        grocery,
+        setGrocery,
+        isLoading,
+        setIsLoading,
+        error,
+        setError,
+    }
+
+    return (
+        <GroceryContext.Provider value={value}>
+            {children}
+        </GroceryContext.Provider>
+    );
+}
+
+export const useGroceryContext = () => {
+    const context = useContext(GroceryContext);
+    if (context === undefined) {
+        throw new Error('useGrocerycontext error. Not used in a grocery provider');
+    }
+    return context;
+}
+
+
+
+
